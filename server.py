5225eabb44c6661c91d47848c0132f295c4c7035
Lautaro Mazzitelli <lmazzitelli@gmail.com>
2013-07-28 16:48:07 +0200

    Simplified server version. Without threads. Just an example.

--- server.py
+++ server.py
@@ -3,42 +3,8 @@
 
 import socket
 import select
-import Queue
-from threading import Thread
-from time import sleep
 from random import randint
 import sys
-
-class ProcessThread(Thread):
-    def __init__(self):
-        super(ProcessThread, self).__init__()
-        self.running = True
-        self.q = Queue.Queue()
-
-    def add(self, data):
-        self.q.put(data)
-
-    def stop(self):
-        self.running = False
-
-    def run(self):
-        q = self.q
-        while self.running:
-            try:
-                # block for 1 second only:
-                value = q.get(block=True, timeout=1)
-                process(value)
-            except Queue.Empty:
-                sys.stdout.write('.')
-                sys.stdout.flush()
-        #
-        if not q.empty():
-            print "Elements left in the queue:"
-            while not q.empty():
-                print q.get()
-
-t = ProcessThread()
-t.start()
 
 def main():
     s = socket.socket()         # Create a socket object
@@ -62,14 +28,6 @@ def main():
         except socket.error, msg:
             print "Socket error! %s" % msg
             break
-    #
-    cleanup()
-
-def cleanup():
-    t.stop()
-    t.join()
-
-#########################################################
 
 if __name__ == "__main__":
     main()
